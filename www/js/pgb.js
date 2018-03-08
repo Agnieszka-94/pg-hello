45823f602fffa37b1667ee3521bdeb1673ed9440
DESKTOP-OM343O8\Agnieszka <agnieszka.t1994@gmail.com>
2018-03-08 21:46:09 +0100

    Motion

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -43,3 +43,18 @@ function checkConnection() {
  
     navigator.notification.alert('Connection type: ' + states[networkState], 'Check connection', 'GOOD!');
 }
+
+function motion() {
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+}
