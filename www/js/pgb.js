ee0252dfe2da40dd4173a69fd5db4f7fbba5e044
DESKTOP-OM343O8\Agnieszka <agnieszka.t1994@gmail.com>
2018-03-13 16:54:02 +0100

    contact

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -62,18 +62,9 @@ navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
 }
 
 function contactlist() {
-    list(success, fail)
-    navigator.contactsPhoneNumbers.list(function(contacts) {
-      console.log(contacts.length + ' contacts found');
-      for(var i = 0; i < contacts.length; i++) {
-         console.log(contacts[i].id + " - " + contacts[i].displayName);
-         for(var j = 0; j < contacts[i].phoneNumbers.length; j++) {
-            var phone = contacts[i].phoneNumbers[j];
-            console.log("===> " + phone.type + "  " + phone.number + " (" + phone.normalizedNumber+ ")");
-         }
-      }
-   }, 
-   function(error) {console.error(error);});
+    window.NativeContactPicker.pickContact(function(e) {
+    console.log("parseLastNineDigits: ", e);
+    });
 }
 
 var app = {
